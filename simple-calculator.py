baef2dbc6e6958247cdf3188383473f887b750ca
Nerya Manassen <neryamanassen9@gmail.com>
2022-01-11 13:33:19 +0200

    first commit

---
+++ simple-calculator.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+root = Tk()
+root.title("Simple Calculator")
+e = Entry(root,width = 35,borderwidth = 5)
+e.grid(row = 0 ,column = 0,columnspan = 3,padx = 10, pady=10)
+
+def button_add():
+    return
+
+button_1 = Button(root,text='1',padx=40,pady=20,command = button_add)
+button_2 = Button(root,text='2',padx=40,pady=20,command = button_add)
+button_3 = Button(root,text='3',padx=40,pady=20,command = button_add)
+button_4 = Button(root,text='4',padx=40,pady=20,command = button_add)
+button_5 = Button(root,text='5',padx=40,pady=20,command = button_add)
+button_6 = Button(root,text='6',padx=40,pady=20,command = button_add)
+button_7 = Button(root,text='7',padx=40,pady=20,command = button_add)
+button_8 = Button(root,text='8',padx=40,pady=20,command = button_add)
+button_9 = Button(root,text='9',padx=40,pady=20,command = button_add)
+button_0 = Button(root,text='10',padx=40,pady=20,command = button_add)
+
+myButton = Button(root,text = "Enter Your Name!",command = myClick,bg = 'blue',fg='red')
+
+
+
+root.mainloop()
